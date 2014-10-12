94375afdd356f2a12886f27d0e1a2c0c6083f931
Denis Kajgorodov <terracott19@gmail.com>
2014-10-13 00:52:36 +0700

    Refactor tests

--- FSM-Lion/LionTest.py
+++ FSM-Lion/LionTest.py
@@ -6,7 +6,6 @@ import unittest
 class LionTest(TestCase):
 
     def test_init(self):
-        #positive tests
         lion = Lion("Hungry")
         self.assertEqual("Hungry", lion.FSM.current_state)
         self.assertIsNone(lion.FSM.prev_state)
@@ -17,43 +16,43 @@ class LionTest(TestCase):
         self.assertIsNone(lion.FSM.prev_state)
         self.assertIsNone(lion.FSM.action)
 
-        #negative tests
         lion = Lion("Crazy")
         self.assertEqual("Crazy", lion.FSM.current_state)  # it's ok, but transition will be not found
         self.assertIsNone(lion.FSM.prev_state)
         self.assertIsNone(lion.FSM.action)
 
-    def test_meet(self):
-        # positive tests
+    def test_meet_antelope(self):
         lion = Lion("Hungry")
         lion.meet("Antelope")
         self.assertEqual("Hungry", lion.FSM.prev_state)
         self.assertEqual("Fed", lion.FSM.current_state)
         self.assertEqual("Eat", lion.FSM.action)
 
-        lion = Lion("Hungry")
-        lion.meet("Hunter")
-        self.assertEqual("Hungry", lion.FSM.prev_state)
-        self.assertEqual("Hungry", lion.FSM.current_state)
-        self.assertEqual("Run away", lion.FSM.action)
-
-        lion = Lion("Hungry")
-        lion.meet("Tree")
-        self.assertEqual("Hungry", lion.FSM.prev_state)
-        self.assertEqual("Hungry", lion.FSM.current_state)
-        self.assertEqual("Sleep", lion.FSM.action)
-
         lion = Lion("Fed")
         lion.meet("Antelope")
         self.assertEqual("Fed", lion.FSM.prev_state)
         self.assertEqual("Hungry", lion.FSM.current_state)
         self.assertEqual("Sleep", lion.FSM.action)
 
+    def test_meet_hunter(self):
+        lion = Lion("Hungry")
+        lion.meet("Hunter")
+        self.assertEqual("Hungry", lion.FSM.prev_state)
+        self.assertEqual("Hungry", lion.FSM.current_state)
+        self.assertEqual("Run away", lion.FSM.action)
+
         lion = Lion("Fed")
         lion.meet("Hunter")
         self.assertEqual("Fed", lion.FSM.prev_state)
         self.assertEqual("Hungry", lion.FSM.current_state)
         self.assertEqual("Run away", lion.FSM.action)
+
+    def test_meet_tree(self):
+        lion = Lion("Hungry")
+        lion.meet("Tree")
+        self.assertEqual("Hungry", lion.FSM.prev_state)
+        self.assertEqual("Hungry", lion.FSM.current_state)
+        self.assertEqual("Sleep", lion.FSM.action)
 
         lion = Lion("Fed")
         lion.meet("Tree")
@@ -61,7 +60,10 @@ class LionTest(TestCase):
         self.assertEqual("Hungry", lion.FSM.current_state)
         self.assertEqual("Look", lion.FSM.action)
 
-        # negative tests
+    def test_meet_tiger(self):
+        lion = Lion("Hungry")
+        self.assertRaises(Exception, lion.meet, "Tiger")
+
         lion = Lion("Fed")
         self.assertRaises(Exception, lion.meet, "Tiger")
 
